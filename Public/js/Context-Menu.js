e328cd6a6d7766e9a31ef53318f3c6e2f716fdf4
Will Simpson <willsimpson237@gmail.com>
2024-02-11 12:24:36 -0500

    Developments 3
    - Started work on the grid system
    - Made context menu look nicer
    -

--- Public/js/Context-Menu.js
+++ Public/js/Context-Menu.js
@@ -1,10 +1,29 @@
+var Utils;
+(function (Utils) {
+    /**Clamps the middle argument between the other 2 arguments */
+    function clamp(min, val, max) {
+        if (val < min)
+            val = min;
+        if (val > max)
+            val = max;
+        return val;
+    }
+    Utils.clamp = clamp;
+})(Utils || (Utils = {}));
 var Main;
 (function (Main) {
-    const Keybinds = {};
+    Main.CellSize = 10;
+    function NewShortcut(Shortcut, func) {
+        Shortcuts[Shortcut] = func;
+    }
+    Main.NewShortcut = NewShortcut;
+    const Shortcuts = {};
     const BuildingObjects = {
         "Main Building": []
     };
     const Buildings = {};
+    const GridLines = [];
+    let InitCalled = false;
     function Init() {
         Main.can = new fabric.Canvas("Can", {
             backgroundColor: "white",
@@ -12,7 +31,37 @@ var Main;
             height: 500,
             stopContextMenu: true,
             fireRightClick: true,
+            fireMiddleClick: true,
         });
+        let GridLineStyle = {
+            stroke: "gray",
+            strokeWidth: Main.CellSize / 10,
+            selectable: false,
+            hoverCursor: "cursor",
+            name: "Can" //Name is can becuase it allowes context menu to click through it 
+        };
+        //draw the grid
+        for (let i = 0; i < window.innerWidth; i += Main.CellSize) {
+            let elem = new fabric.Line([i, 0, i, window.innerHeight], GridLineStyle);
+            GridLines.push(elem);
+            Main.can.add(elem);
+        }
+        for (let i = 0; i < window.innerHeight; i += Main.CellSize) {
+            let elem = new fabric.Line([0, i, window.innerWidth, i], GridLineStyle);
+            GridLines.push(elem);
+            Main.can.add(elem);
+        }
+        //canvas Events 
+        Main.can.on("mouse:wheel", function (opt) {
+            var delta = opt.e.deltaY;
+            var zoom = Main.can.getZoom();
+            zoom *= .999 ** delta;
+            zoom = Utils.clamp(0.1, zoom, 2);
+            Main.can.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+            opt.e.preventDefault();
+            opt.e.stopPropagation();
+        });
+        //Window resizing 
         let TimerID = undefined;
         ResizeCanvas();
         window.addEventListener("resize", ev => {
@@ -21,6 +70,7 @@ var Main;
             TimerID = setTimeout(ResizeCanvas, 500);
         });
         ContextMenu.Init();
+        InitCalled = true;
     }
     Main.Init = Init;
     function ResizeCanvas() {
@@ -28,6 +78,12 @@ var Main;
         Main.can.setHeight(window.innerHeight);
     }
     document.addEventListener("keydown", ev => {
+        let KeyCode = "";
+        if (ev.key != "Shift" && ev.key != "Control") {
+            KeyCode = ev.shiftKey ? "Shift+" : "" + ev.ctrlKey ? "Ctrl+" : "" + ev.key;
+            if (Shortcuts[KeyCode])
+                Shortcuts[KeyCode]();
+        }
     });
 })(Main || (Main = {}));
 var ContextMenu;
@@ -46,7 +102,7 @@ var ContextMenu;
     ContextMenu.ShowContextMenu = ShowContextMenu;
     const Actions = {};
     function CreateAction(Context, name, func) {
-        let elem = document.createElement("div");
+        let elem = document.createElement("span");
         elem.classList.add("Context-Menu-Item");
         elem.innerText = name;
         elem.onclick = () => {
@@ -57,14 +113,16 @@ var ContextMenu;
             Actions[Context] = [[name, elem]];
         else
             Actions[Context].push([name, elem]);
-        console.log(Actions);
     }
     ContextMenu.CreateAction = CreateAction;
     function Init() {
         TargetMenu = document.getElementById("ContextMenu");
         document.body.appendChild(TargetMenu);
         Main.can.on("mouse:down", function (opt) {
+            console.log(opt.target);
             ContextMenuIsOver_Element = opt.target;
+            if (opt.e.buttons == 4)
+                opt.e.preventDefault();
             if (opt.button == 3) {
                 let ev = opt.e;
                 let x = opt.e.clientX;
@@ -76,12 +134,15 @@ var ContextMenu;
                     ContextMenuIsOver = "Can";
                 if (Actions[ContextMenuIsOver]) {
                     let inital = Array.from(TargetMenu.children);
-                    for (const Action of Actions[ContextMenuIsOver]) {
-                        TargetMenu.appendChild(Action[1]);
-                    }
                     for (const e of inital) {
                         TargetMenu.removeChild(e);
                     }
+                    for (const Action of Actions[ContextMenuIsOver]) {
+                        TargetMenu.appendChild(Action[1]); //Actions 1 is the element 
+                    }
+                }
+                else {
+                    return;
                 }
                 if (window.innerWidth - TargetMenu.clientWidth - x < 0) {
                     x -= TargetMenu.clientWidth;
@@ -97,8 +158,6 @@ var ContextMenu;
             else {
                 HideContextMenu();
             }
-        }).on("object:scaling", function (opt) {
-            console.log(opt.target.name);
         });
         document.addEventListener("contextmenu", (ev) => {
             if (ev.target.parentElement == TargetMenu)
@@ -111,8 +170,8 @@ var ContextMenu;
 Main.Init();
 ContextMenu.CreateAction("Can", "Create Building", () => {
     let elem = new fabric.Rect({
-        width: 25,
-        height: 25,
+        width: Main.CellSize,
+        height: Main.CellSize,
         lockUniScaling: true,
     });
     elem.name = "Building";
--- typescript/Context-Menu.ts
+++ typescript/Context-Menu.ts
@@ -2,13 +2,39 @@ type ContextMenuFunc = (ev : Event) => any
 interface MouseEV extends MouseEvent {
     target : HTMLElement | undefined
 } 
+namespace Utils {
+    /**Clamps the middle argument between the other 2 arguments */
+    export function clamp(min : number, val : number, max : number) : number {
+        if(val < min) val = min;
+        if(val > max) val = max;
+        return val;
+    }
+}
+
+
+
+
+
 namespace Main {
-    const Keybinds = {};
+    export let CellSize = 10;
+
+    export function NewShortcut(Shortcut : string, func : () => {}) {
+        Shortcuts[Shortcut] = func;
+    }
+    const Shortcuts = {};
     const BuildingObjects = {
         "Main Building" : []
     }
     const Buildings = {};
-    
+    const GridLines = [];
+    let InitCalled = false;
+
+
+
+
+
+
+
     export let can : fabric.Canvas;
     export function Init() {
         can = new fabric.Canvas("Can", {
@@ -17,8 +43,60 @@ namespace Main {
             height : 500,
             stopContextMenu : true,
             fireRightClick : true,
+            fireMiddleClick : true,
         })
-        
+
+
+        let GridLineStyle  = {
+            stroke : "gray",
+            strokeWidth : CellSize / 10,
+            selectable : false,
+            hoverCursor : "cursor",
+            name : "Can" //Name is can becuase it allowes context menu to click through it 
+        }
+        //draw the grid
+        for(let i = 0; i < window.innerWidth; i += CellSize) {
+            let elem = new fabric.Line([i, 0, i, window.innerHeight], GridLineStyle)
+            GridLines.push(elem)
+            can.add(elem);
+        }
+        for(let i = 0; i < window.innerHeight; i += CellSize) {
+            let elem = new fabric.Line([0, i, window.innerWidth, i], GridLineStyle)
+            GridLines.push(elem);
+            can.add(elem);
+        }
+
+
+
+
+
+
+
+        //canvas Events 
+        can.on("mouse:wheel", function(opt) {
+            var delta = opt.e.deltaY
+            var zoom = can.getZoom();
+            zoom *= .999 ** delta;
+            zoom = Utils.clamp(0.1, zoom, 2);
+            can.zoomToPoint({x : opt.e.offsetX, y : opt.e.offsetY}, zoom)
+            opt.e.preventDefault();
+            opt.e.stopPropagation();
+
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+        //Window resizing 
         let TimerID = undefined
         ResizeCanvas()
         window.addEventListener("resize", ev => {
@@ -27,13 +105,19 @@ namespace Main {
         })
 
         ContextMenu.Init();
+        InitCalled = true;
     }
     function ResizeCanvas() {
         can.setWidth(window.innerWidth);
         can.setHeight(window.innerHeight);
     }
-    document.addEventListener("keydown", ev => {
 
+    document.addEventListener("keydown", ev => {
+        let KeyCode = ""
+        if(ev.key != "Shift" && ev.key != "Control") {
+            KeyCode = ev.shiftKey ? "Shift+" : "" + ev.ctrlKey ? "Ctrl+" : "" + ev.key
+            if(Shortcuts[KeyCode]) Shortcuts[KeyCode]();
+        }
     })
 }
 namespace ContextMenu {
@@ -41,8 +125,12 @@ namespace ContextMenu {
     let ContextMenuIsOver : string;
     let TargetMenu : HTMLElement;
     let IsNewContext : boolean;
+
     interface I_Actions {
         [TargetName : string | undefined] : [[string, HTMLElement]]
+    }
+    interface I_ActionOptions  {
+        
     }
 
 
@@ -56,7 +144,7 @@ namespace ContextMenu {
     }
     const Actions : I_Actions = {};
     export function CreateAction(Context : string, name : string, func : (target : fabric.Object) => any) {
-        let elem = document.createElement("div");
+        let elem = document.createElement("span");
         elem.classList.add("Context-Menu-Item")
         elem.innerText = name;
         elem.onclick = () => {
@@ -65,7 +153,6 @@ namespace ContextMenu {
         }
         if(!Actions[Context]) Actions[Context] = [[name, elem]]
         else Actions[Context].push([name, elem]);
-        console.log(Actions);
         
         
 
@@ -73,13 +160,16 @@ namespace ContextMenu {
     }
 
 
-
     export function Init() {
         TargetMenu = document.getElementById("ContextMenu");
         
         document.body.appendChild(TargetMenu)
         Main.can.on("mouse:down",function(opt) {
+
+            console.log(opt.target);
+            
             ContextMenuIsOver_Element = opt.target    
+            if(opt.e.buttons == 4) opt.e.preventDefault();
             if(opt.button == 3) {
                 let ev = opt.e;
                 let x = opt.e.clientX
@@ -93,11 +183,13 @@ namespace ContextMenu {
                 } else ContextMenuIsOver = "Can";
                 if(Actions[ContextMenuIsOver]) {
                     let inital = Array.from(TargetMenu.children);
-                    for(const Action of Actions[ContextMenuIsOver]) {
-                        TargetMenu.appendChild(Action[1])  
-                    }
                     for(const e of inital) {TargetMenu.removeChild(e);}
-                } 
+                    for(const Action of Actions[ContextMenuIsOver]) {
+                        
+                        TargetMenu.appendChild(Action[1])  //Actions 1 is the element 
+                    }
+
+                } else {return;}
                 
                 if(window.innerWidth - TargetMenu.clientWidth - x < 0) {
                     x -= TargetMenu.clientWidth;
@@ -114,28 +206,32 @@ namespace ContextMenu {
                 HideContextMenu();
             }
 
-        }).on("object:scaling", function(opt)  {
-            console.log(opt.target.name);
-            
         })
         document.addEventListener("contextmenu", (ev : MouseEV) => {
             if(ev.target.parentElement == TargetMenu) ev.preventDefault();
-            
-            })
+        })
+
         TargetMenu.classList.add("HideContextMenu");
             
     }
 
 }
 Main.Init();
+
+
+
+
+
+
+
+
+
 ContextMenu.CreateAction("Can","Create Building", () => {
     let elem = new fabric.Rect({
-        width : 25,
-        height : 25,
-        lockUniScaling : true,
-        
+        width : Main.CellSize,
+        height : Main.CellSize,
+        lockUniScaling : true,  
     })
-    
     elem.name = "Building"
     Main.can.add(elem);
 })
